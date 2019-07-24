09b206ff0782edd10258b647038fd3f3c591b934
mcrey fonacier <40071594+polpokpol@users.noreply.github.com>
2019-07-25 01:04:51 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+class App{
+
+    public static void main(String[] args) {
+        String text = "hello world";
+        String pattern = "world";
+
+        // Bf bf = new Bf();
+
+        System.out.println(Bf.search(text, pattern));
+    }
+
+
+}
